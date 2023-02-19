b84eb3e1339557c50af2771af2f775f965bf9670
MVinicyuss <118633498+MVinicyuss@users.noreply.github.com>
2023-02-19 17:12:05 -0300

    .

---
+++ src/Components/Button/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding:10px;
+    border: 1px solid #CDCDCD;
+    background-color:rgb(61, 236, 45);
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &: hover{
+        opacity: 0.6;
+    }
+`
---
+++ src/Components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%
+    height: 75px;
+    background-color:rgb(43, 60, 214);
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px
+    font-family: 'Roboto';
+
+
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color:rgb(43, 60, 214);
+        border: 0;
+        align-items: flex-end;
+
+        padding: 0 5px;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+
+        color: white;
+    }
+`
